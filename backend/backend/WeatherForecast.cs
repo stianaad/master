ccccea48e35d849ebd522f8d4745aa549e27e8bb
Stian <stian_adnanes@hotmail.com>
2022-01-24 11:38:17 +0100

    add backend

--- backend/backend/WeatherForecast.cs
+++ backend/backend/WeatherForecast.cs
@@ -1,3 +1,5 @@
+﻿using System;
+
 namespace backend
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace backend
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
