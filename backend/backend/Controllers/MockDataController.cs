157dcdbabe5198340e0dd33c7cb4c6497aef63df
andergst <andergst@stud.ntnu.no>
2022-01-27 13:44:27 +0100

    Added fetch for a tour

--- backend/backend/Controllers/MockDataController.cs
+++ backend/backend/Controllers/MockDataController.cs
@@ -7,6 +7,7 @@ using backend.Models;
 using backend.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
@@ -26,13 +27,32 @@ namespace backend.Controllers
             var tourFile = _files.GetFile("Tour");
             var tourLocationFile = _files.GetFile("TourLocations");
             var sheepLocationFile = _files.GetFile("SheepPositions");
-            TourData tour = FileUtils.ReadTourDataFromFiles(tourFile, tourLocationFile, sheepLocationFile);
+            TourData tour = FileUtils.ReadTourDataFromFiles(tourFile, tourLocationFile, sheepLocationFile, 5);
             if (tour != null)
             {
                 _context.Tours.Add(tour);
                 await _context.SaveChangesAsync();
             }
             return Ok();
+        }
+
+        [HttpGet("api/GetTour/{id}")]
+        public async Task<ActionResult<TourData>> GetTour(int id)
+        {
+            var tour = await _context.Tours.FindAsync(id);
+
+            if (tour == null)
+            {
+                return NotFound();
+            }
+
+            return tour;
+        }
+
+        [HttpGet("api/GetTours")]
+        public async Task<ActionResult<IEnumerable<TourData>>> GetTours()
+        {
+            return await _context.Tours.Include(t => t.SheepPositions).Include(t => t.Positions).ToListAsync();
         }
     }
 }
